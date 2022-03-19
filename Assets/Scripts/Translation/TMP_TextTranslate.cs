cdc116020b4e0c395680983acb6ffcc0bb4969e8
Christian Chaux <christian.chaux@bionomeex.com>
2022-03-19 01:01:04 +0100

    Add translation system

---
+++ Assets/Scripts/Translation/TMP_TextTranslate.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+namespace ScientificGameJam.Translation
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TMP_TextTranslate : MonoBehaviour
+    {
+        private string _original;
+
+        private void Start()
+        {
+            _original = GetComponent<TMP_Text>().text;
+            UpdateText();
+        }
+
+        public void UpdateText()
+        {
+            var sentence = _original;
+            foreach (var match in Regex.Matches(_original, "{([^}]+)}").Cast<Match>())
+            {
+                sentence = sentence.Replace(match.Value, Translate.Instance.Tr(match.Groups[1].Value));
+            }
+            GetComponent<TMP_Text>().text = sentence;
+        }
+    }
+}
