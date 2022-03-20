9de78cf794bce635e4b0347ada57f665a3ab393e
nethos <geeklego@gmail.com>
2022-03-20 13:10:38 +0100

    main menu, settings and credits glow up

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGameButton()
+    [Header("Canvas")]
+    public GameObject menuCanvas;
+    public GameObject settingsCanvas;
+    public GameObject creditsCanvas;
+
+    [Header("Language buttons")]
+    public Image frButton;
+    public Image enButton;
+
+    public void StartGame()
     {
-        Debug.Log("Starting the game !");
+        SceneManager.LoadScene(1);
     }
 
-    public void ToSettingsButton()
+    public void ToMenu()
     {
-        Debug.Log("Launching options !");
+        menuCanvas.SetActive(true);
+        settingsCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
     }
 
-    public void ToCreditsButton()
+    public void ToSettings()
     {
-        Debug.Log("These are the credits !");
+        menuCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+    }
+
+    public void ToCredits()
+    {
+        menuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void SetLanguage(string language)
+    {
+        if (language == "fr")
+        {
+            enButton.color = new Vector4(1, 1, 1, 0.5f);
+            frButton.color = new Vector4(1, 1, 1, 1f);
+        }
+        else
+        {
+            enButton.color = new Vector4(1, 1, 1, 1f);
+            frButton.color = new Vector4(1, 1, 1, 0.5f);
+        }
     }
 }
